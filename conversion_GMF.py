fixed bug in calculation of Psi

conversion_GMF.py
@@ -258,10 +258,9 @@ class PhotALPs_GMF(PhotALPs_ICM):
 	m		= self.B > 0.
 
 
-	# Psi = arctan( B_transversal / B_along prop. direction)
+	self.Psin[m]	= np.arctan2(Bt[m],Bu[m])	# arctan2 selects the right quadrant
 	# Debug:
-	#self.Psin[m]	= np.arctan2(self.B[m],Bs[m])	# arctan2 selects the right quadrant
-	self.Psin[m]	= np.ones((self.Psin[m]).shape[0]) * np.pi / 4.
+	#self.Psin[m]	= np.ones((self.Psin[m]).shape[0]) * np.pi / 4.
 
 	self.T1		= np.zeros((3,3,self.Nd),np.complex)
 	self.T2		= np.zeros((3,3,self.Nd),np.complex)

conversion_ICM.py
@@ -161,7 +161,7 @@ class PhotALPs_ICM(object):
 	    self.bfield.new_random_numbers()		# new random numbers
 	    Bu		= self.bfield.Bgaus(self.r)	# calculate second transverse component
 	    self.B	= np.sqrt(Bt ** 2. + Bu ** 2.)	# calculate total transverse component 
-	    self.Psin	= np.arccos(Bu / self.B)	# and angle to x2 (t) axis
+	    self.Psin	= np.arctan2(Bt , Bu)		# and angle to x2 (t) axis -- use atan2 to get the quadrants right
 
 	if self.Bn_const:
 	    self.n		= self.n * np.ones(int(self.Nd))	# assuming a constant electron density over all domains